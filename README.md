ad272135e6d249489f310a497251920820626ca5
Andrey Matveev <andrey.g.matveev@gmail.com>
2015-10-18 14:31:03 +0700

    coplex refactoring, fixer.io

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# jquery-inline-currency
-Simple jQuery plugin for showing prices in different currencies using actual exchange rates (via Yahoo Currency API)
+# jQuery.inline-currency
+Simple jQuery plugin for showing prices in different currencies using actual exchange rates.
+
+# Exchange rates source
+
+In the begining it was Yahoo Currencies Service, but in the discussion at `https://developer.yahoo.com/forum/General-Discussion-at-YDN/Using-Yahoo-Finance-API-Not-RSS-/1250246255000-0b82f8f0-7f48-3af2-8fe2-e73a138cbfaa/` it was mentioned:
+
+> The reason for the lack of documentation is that we don't have a Finance API. It appears some have reverse engineered an API that they use to pull Finance data, but they are breaking our Terms of Service (no redistribution of Finance data) in doing this so I would encourage you to avoid using these webservices.
+>
+>Cheers,
+>Robyn Tippins
+>Community Manager, YDN
+
+So, I've decided not to break Yahoo's Terms of Service and have found http://fixer.io/ (https://github.com/hakanensari/fixer-io/). As it is free and has MIT License, it's the best choice for our purposes. Thanks, @hakanensari
+
+# How often do rates update
+
+Once a day (for now, October 2015). Check https://github.com/hakanensari/fixer-io/ for more info.
+
+# Browser compatibility
+You can check `window.localStorage` use at http://caniuse.com/#search=localstorage
--- js/jquery.inline-currency.js
+++ js/jquery.inline-currency.js
@@ -1,100 +1,87 @@
 (function ($) {
 
   var LS_NAME = "$$jqueryInlineCurrency$$",
-    LS_RATES = null,
-    LS_SCRIPT_ELEMENT = null,
-    LS_CURRENCIES = null;
+    LS_EXCHANGE = null;
 
   var defaults = {
-    "currencies": "usd, rub, eur",
-    "currency": "usd",
-    "convertTo": "rub, eur",
-    "thousandsSplit": ",",
+    "currency": "RUB",
+    "convertTo": "USD, EUR",
+    "thousandsSplit": "",
     "decimalsSplit": ".",
-    "updateInterval": 3600,
+    "containerElement": "p",
+    "containerClass": "jic-container",
+    "rateElement": "span",
+    "rateClass": "jic-rate",
+    "currencyElement": "span",
+    "currencyClass": "jic-currency",
     "debug": true
-  };
-
-  /**
-   * 1. Read data from Yahoo Currency API
-   * 2. Save data in the local storage
-   * 3. Update rates variable
-   *
-   * @param  {Object} data Data from API
-   */
-  $.inlineCurrencyUpdate = function (data) {
-    var ratesData = {};
-    if (data && data.query && data.query.results) {
-      ratesData = {
-        created: new Date(data.query.created).getTime(),
-        currencies: LS_CURRENCIES,
-        data: {}
-      }
-      if (data.query.count > 1) {
-        for (var index in data.query.results.rate) {
-          ratesData.data[data.query.results.rate[index].id] = parseFloat(data.query.results.rate[index].Rate, 10);
-        }
-      } else if (data.query.count == 1) {
-        ratesData.data[data.query.results.rate.id] = parseFloat(data.query.results.rate.Rate, 10);
-      }
-    }
-    window.localStorage.setItem(LS_NAME, JSON.stringify(ratesData));
-
-    LS_RATES = ratesData;
   };
 
   $.fn.inlineCurrency = function (options) {
     options = $.extend(defaults, options);
-    LS_RATES = getRates();
+    options.convertTo = getListIfCommaString(options.convertTo);
+    if (options.convertTo) {
+      options.convertTo = options.convertTo.sort();
+    } else {
+      debugMsg('EXIT: options.convertTo must be specified');
+      return false;
+    }
+    if (!options.currency) {
+      debugMsg('EXIT: options.currency must be specified');
+      return false;
+    }
 
-    /**
-     * Get rates from the local storage or get them form Yahoo Service API
-     * @return {Object} Rates Object
-     */
-    function getRates() {
-      var rates = JSON.parse(window.localStorage.getItem(LS_NAME));
+    function updateRates(callback) {
+      var ex = JSON.parse(window.localStorage.getItem(LS_NAME));
 
-      if (rates && rates.created < new Date().getTime() - options.updateInterval) {
-        debugMsg('remove local exchange rates')
+      if (!ex || ex && (!ex.rates || !ex.created ||
+          ex.created < new Date().getTime() - 86400000)) {
+
+        // @TODO: remove
+
+        debugMsg('remove local exchange rates');
         window.localStorage.removeItem(LS_NAME);
-        rates = null;
+        ex = {};
+      } else {
+        LS_EXCHANGE = ex;
+        if(typeof(callback) == 'function') {
+          return callback();
+        }
+        else {
+          return false;
+        }
       }
 
-      options.currencies = getListIfCommaString(options.currencies);
+      $.getJSON('http://api.fixer.io/latest', {
+          base: options.currency,
+          symbols: options.convertTo.join(',')
+        },
+        function (data) {
+          if (data.rates) {
+            ex.created = new Date().getTime();
+            data.rates[options.currency] = 1;
+            ex.rates = data.rates;
+            window.localStorage.setItem(LS_NAME, JSON.stringify(ex));
+            debugMsg('OK: rates updated')
+            LS_EXCHANGE = ex;
 
-      if(options.currencies) {
-        options.currencies = options.currencies.sort();
-      }
-
-      if (!rates ||
-        !rates.currencies ||
-        rates.currencies != options.currencies) {
-
-        debugMsg('get new exchange rates', 'asdfasdf')
-
-        var currencyPairs = "";
-        if (options.currencies.length > 2) {
-          for (var i = 0; i < options.currencies.length - 1; i++) {
-            for (var j = i + 1; j < options.currencies.length; j++) {
-              currencyPairs += (i == 0 && j == i + 1 ? "" : "%2C") + "%22" + options.currencies[i] + options.currencies[j] + "%22%2C%22" + options.currencies[j] + options.currencies[i] + "%22";
+            if(typeof(callback) == 'function') {
+              return callback();
+            }
+            else {
+              return false;
             }
           }
-        }
-        if (currencyPairs) {
-          LS_CURRENCIES = options.currencies.join(",");
-
-          LS_SCRIPT_ELEMENT = document.createElement("script");
-          LS_SCRIPT_ELEMENT.setAttribute("src", "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(" + currencyPairs + ")%20&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=$.inlineCurrencyUpdate");
-          document.body.appendChild(LS_SCRIPT_ELEMENT);
-        }
-      };
-
-      return rates;
+        },
+        function () {
+          debugMsg('EXIT: service temporary unaviable')
+          return false;
+        });
     };
 
     function debugMsg() {
-      if(options.debug) {
-        if(! console) {
+      if (options.debug) {
+        if (!console) {
           window.console = {};
         }
         console.log('[jQueryInlineCurrency: ', $.makeArray(arguments).join(', '), ']');
@@ -102,37 +89,41 @@
     }
 
     function getListIfCommaString(value) {
-      if(value) {
-        if(typeof(value) != 'object') {
-          return value.split(",");
-        }
-        else {
+      if (value) {
+        if (typeof (value) != 'object') {
+          return value.split(/,\s/);
+        } else {
           return value;
         }
-      }
-      else {
-        return false;
-      }
-    }
-
-    function convertCurrency(value, from, to) {
-      if (from != to && LS_RATES.data[(from + "" + to).toUpperCase()]) {
-        return Math.round(value * LS_RATES.data[(from + "" + to).toUpperCase()] * 100) / 100;
       } else {
         return false;
       }
     }
 
-    function getValueFromString($object) {
+    function convertCurrency(value, from, to) {
+      value = value || 0;
+      if (LS_EXCHANGE &&
+          LS_EXCHANGE.rates &&
+          to &&
+          LS_EXCHANGE.rates[to.toUpperCase()] &&
+          from &&
+          LS_EXCHANGE.rates[from.toUpperCase()]) {
+        return Math.round(value / LS_EXCHANGE.rates[from.toUpperCase()] * LS_EXCHANGE.rates[to.toUpperCase()] * 100) / 100;
+      } else {
+        return false;
+      }
+    }
+
+    function getNumberFromText($object) {
       var text = $object.text();
       var matches = new RegExp('(\\d+' + (options.thousandsSplit ? '(?:\\' + options.thousandsSplit + '\\d+)*' : '') + '(?:\\' + options.decimalsSplit + '\\d+)?)+').exec(text);
       return matches && matches.length ? matches[0] : 0;
     }
 
-    function getResult($object) {
+    function printObjectCurrencies($object) {
       var str = "",
         originalStr = $object.html(),
-        value = $object.data("jic-value") || getValueFromString($object),
+        value = $object.data("jic-value") || getNumberFromText($object),
         currency = $object.data("jic-currency") || options.currency,
         convertTo = getListIfCommaString($object.data("jic-convert-to") || options.convertTo);
 
@@ -141,22 +132,40 @@
         currency = $.trim(currency);
 
         var val = convertCurrency(value, currency, convertTo[i]);
-        str += val !== false ? ((!str ? "" : ", ") + val + " " + convertTo[i].toUpperCase()) : "";
+        if(val) {
+          if(options.rateElement) {
+            str += '<' + options.rateElement +
+              (options.rateClass ? ' class="' + options.rateClass + '"' : '') + '>' +
+              val + ' ' +
+              (options.currencyElement ? '<' + options.currencyElement +
+              (options.currencyClass ? ' class="' + options.currencyClass + '"' : '') +
+              '>' : '') +
+              convertTo[i].toUpperCase() +
+              (options.currencyElement ? '</' + options.currencyElement + '>' :'') +
+              '</' + options.rateElement + '>';
+          }
+          else if(options.currencySplit) {
+            str += (!str ? "" : ", ") + val + ' ' + convertTo[i].toUpperCase();
+          }
+        }
       }
 
-      $object.html(originalStr + (str ? " (" + str + ")" : ""));
+      if(str) {
+        if(options.containerElement) {
+          str = '<' + options.containerElement +
+            (options.containerClass ? ' class="' + options.containerClass + '"' : '') + '>' +
+            str +
+            '</' + options.containerElement + '>';
+        }
+        $object.append($(str));
+      }
     }
 
     return this.each(function () {
       var $this = $(this);
-
-      if (LS_SCRIPT_ELEMENT) {
-        $(LS_SCRIPT_ELEMENT).load(function () {
-          getResult($this);
-        });
-      } else {
-        getResult($this);
-      }
+      updateRates(function () {
+        printObjectCurrencies($this);
+      })
     });
   };
 })(jQuery);
